cbba1d09e855de61c76ee0a90e90163149161c36
Ryan Lynch <lynch.r.j@gmail.com>
2019-12-23 12:50:38 -0500

    Added volcano

--- src/entities/inanimate/volcano.tsx
+++ src/entities/inanimate/volcano.tsx
@@ -3,34 +3,32 @@ import { useRandomEmoji, useScale, useAction } from '../../core/logic'
 import { Square, PositionProps } from '../../core/square'
 import { story } from '../../core/utils'
 
-export const Mountain: React.FunctionComponent<PositionProps> = ({
+export const Volcano: React.FunctionComponent<PositionProps> = ({
   position,
   self,
-  state,
   id,
+  state,
 }) => {
   const { act, behave } = useAction(position, state, id)
 
-  behave(({ create, replace }) => {
-    if (Math.random() < 0.1) {
-      const volcano = create('Volcano')
+  behave(({ look, create, replace }) => {
+    const flammable = look('Space') || look('Tree')
+    const fire = create('Fire')
 
-      replace(story`${self} erupted into ${volcano}`, self, volcano)
-    }
-  }, 100)
+    if (flammable) replace(story`${self} spewed ${fire}`, flammable, fire)
+  }, 1)
 
   return (
     <Square
       title={story`${self}`}
       scale={useScale(() => Math.random() + 3)}
-      background="rgba(100, 100, 100, 0.5)"
+      background="rgba(255, 100, 100, 0.5)"
       position={position}
       onClick={act(({ replace, create }) => {
-        const volcano = create('Volcano')
-        replace(story`${self} turned into volcano`, self, volcano)
+        replace(story`${self} removed`, self, create('Space'))
       })}
     >
-      {useRandomEmoji('⛰', '🏔')}
+      {useRandomEmoji('🌋')}
     </Square>
   )
 }
